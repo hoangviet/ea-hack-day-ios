0851805ee46751b7aadfdcd48439711a6f8cfbcc
Viet Nguyen <ho@ngviet.com>
2015-09-27 02:50:31 +0700

    UI fixes

--- ChungKhoan/Views/StickerInfoCollectionViewCell.swift
+++ ChungKhoan/Views/StickerInfoCollectionViewCell.swift
@@ -2,18 +2,16 @@ import UIKit
 
 class StickerInfoCollectionViewCell: UICollectionViewCell {
     @IBOutlet weak var companyNameLabel: UILabel!
-    @IBOutlet weak var companyDomainLabel: UILabel!
     @IBOutlet weak var priceLabel: UILabel!
     @IBOutlet weak var changeLabel: UILabel!
     @IBOutlet weak var changePercentLabel: UILabel!
     
     func configure(#companyName: String, companyDomain: String, price: CFloat, change: CFloat, changePercent: CFloat) {
-        companyNameLabel.text   = companyName
-        companyDomainLabel.text = companyDomain
+        companyNameLabel.text = companyName
         if (price == 0) {
             priceLabel.text = "--"
         } else {
-            priceLabel.text = "\(price)đ"
+            priceLabel.text = "\(NSNumber(float: price).currency)"
         }
         changeLabel.text        = "\(change)"
         changePercentLabel.text = "\(changePercent)%"
